61c1ae89675207a16c1fd9d6f5dac38d27de6d1d
Enric <enriccc15@gmail.com>
2021-01-30 12:22:38 +0100

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Count from './Components/Count';
+
+// 1. Las clases se llaman className
+// 2. TODAS las etiquetas tienen que cerrarse
+// 3. Solo podemos devolver un elemento padre
+const App = () => {
+    const renderCounts = () => {
+        return [1, 2, 3, 4, 5, 6].map(number => {
+            return <Count name={number} />;
+        });
+    };
+
+    return <div>{renderCounts()}</div>;
+};
 
 export default App;
---
+++ src/Components/Count.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './count.css';
+
+// Los props son SOLO LECTURA
+const Count = props => {
+    // La parte izquierda es el estado
+    // La parte derecha es el modificador
+    // El estado se modifica solo con el modificador de estado
+    const [count, setCount] = useState(0);
+
+    const showCount = () => {
+        // Los cambios de estado son ASINCRONOS
+        setCount(count + 5);
+        alert(count);
+    };
+
+    return (
+        <div className="count">
+            <div>{props.name}</div>
+            <div>{props.address}</div>
+            <div>{count}</div>
+            <button onClick={() => setCount(count + 1)}>Incrementar</button>
+            <button onClick={() => setCount(count - 1)}>Decrementar</button>
+            <hr />
+            <button onClick={() => showCount()}>Show alert</button>
+        </div>
+    );
+};
+
+export default Count;
